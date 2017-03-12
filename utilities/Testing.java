6e0e9929e1203bb0899507a2d9038d40503e6990
nsampemane <neel.sampemane@gmail.com>
2017-03-12 00:56:03 -0800

    ADDED Cellular Automata - can generate levels pseudo-randomly

--- utilities/Testing.java
+++ utilities/Testing.java
@@ -1,15 +1,31 @@
 package utilities;
+
 import java.util.Arrays;
+
+import generation.CellularAutomata;
 
 
 import display.TileScreen;
 
-public class Testing {
-    public static  void main(String[] args) throws java.io.IOException{
 
-        TileScreen x = new TileScreen("Penguicide", 16, 16, 32);
-        FlatArrayTools tools = new FlatArrayTools(3,3);
-        System.out.println(Arrays.toString(tools.getDirectAdjacencies(4)));
+public class Testing {
+    private final static int COLUMNS = 20;
+    private final static int ROWS = 20;
+
+    public static void main(String[] args) throws java.io.IOException, java.lang.InterruptedException{
+
+        TileScreen x = new TileScreen("Penguicide", COLUMNS, ROWS, 32);
+        CellularAutomata cells = new CellularAutomata(COLUMNS, ROWS, 4, 5, .45);
+        for (int z = 1; z < 10; z++) {
+            for (int i = 0; i < COLUMNS * ROWS; i++) {
+
+                int materialCode =  cells.getGrid()[i] ? 1 : 0;
+                x.setMaterial(i, materialCode);
+            }
+            x.refresh();
+            cells.doCycle();
+            Thread.sleep(3000);
+        }
 
     }
 
